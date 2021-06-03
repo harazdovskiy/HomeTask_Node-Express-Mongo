15f3730e7dba65d90bfa944705c1c37fd54ede27
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2021-06-04 00:27:07 +0300

    Update addTodo.js

--- middleware/addTodo.js
+++ middleware/addTodo.js
@@ -1,5 +1,7 @@
 const TodoModel = require('../models/todoList')
 
+// It's better to use Model-Routes-Controllers-Services Directory Structure, and use middlewares only for data validation
+// check this example https://sodocumentation.net/node-js/topic/10785/route-controller-service-structure-for-expressjs
 module.exports = async (req, res, next) => {
     try {
         req.data = await TodoModel.create(req.body)
