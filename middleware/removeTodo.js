11a17672f287c42e012c2635fbb7ca33ce686ed3
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2021-06-04 00:49:01 +0300

    Update removeTodo.js

--- middleware/removeTodo.js
+++ middleware/removeTodo.js
@@ -1,16 +1,22 @@
 const TodoModel = require('../models/todoList')
 
+// Example of code improvements
 module.exports = async (req, res, next) => {
     try {
-        const todoId = req.params.id
-        const presentId = await TodoModel.findById(todoId).exec()
-        if (presentId) {
-            req.data = await TodoModel.deleteOne({ "_id": todoId})
-        } else {
-            return res.send('There is no todo with this id!')
+        
+        const {id: todoId} = req.params
+
+        if(!todoId) res.status(400).json({ error: 'Todo id is not provided!' })
+
+        const todo = await TodoModel.deleteOne({_id: todoId})
+
+        if(!todo) {
+            //handle error
         }
+           //handle success
+        
     } catch (e) {
-        res.status(500).json(e)
+        res.status(500).json(error: e)
     }
     next()
 }
