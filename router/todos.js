33435f2ae08c6f1cd560bbe29073a41bc37470cc
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2021-06-04 00:31:14 +0300

    Update todos.js

--- router/todos.js
+++ router/todos.js
@@ -6,6 +6,8 @@ const addTodo = require('../middleware/addTodo')
 const removeTodo = require('../middleware/removeTodo')
 const toggleCompleted = require('../middleware/toggleCompleted')
 
+// Router should consists of middlewares that validate your request and controller that accepts the data and uses service as a business logic
+// another good example is here https://www.coreycleary.me/project-structure-for-an-express-rest-api-when-there-is-no-standard-way
 router.get('/', 
     getTodo,
     (req, res) => {
